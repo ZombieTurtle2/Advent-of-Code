d9040e635a84ef956bbc4b979e2895347285a422
ZombieTurtle2 <tyler.wlcx@gmail.com>
2023-02-01 14:14:06 -0700

    Attempting 3b

--- 2022/day3/3b.js
+++ 2022/day3/3b.js
@@ -12,23 +12,39 @@ let lines = fs
     .split('\r\n');
 let total = 0
 
-for (let line of lines) {
-    let length = line.length;
-    let left = line.slice(0, length / 2)
-    let right = line.slice(length/2)
+// for (let line of lines) {
+//     let length = line.length;
+//     let left = line.slice(0, length / 2)
+//     let right = line.slice(length/2)
 
-    for (let char of left) {
-        if (right.includes(char)) {
-            console.log(char)
-            total = total + priorities.indexOf(char)
-            console.log(total)
-            break
-        }
+//     for (let char of left) {
+//         if (right.includes(char)) {
+//             console.log(char)
+//             total = total + priorities.indexOf(char)
+//             console.log(total)
+//             break
+//         }
+//     }
+//     console.log("")
+// }
+
+const groupSize = 3;
+const elfGrouping = [];
+
+for (let i = 0; i < lines.length; i += groupSize) {
+    const elfGroup = lines.slice(i, i + groupSize);
+    elfGrouping.push(elfGroup);
+
+for (let char of elfGroup) {
+    if (elfGroup.includes(char)) {
+        console.log(char)
+        total = total + priorities.indexOf(char)
+        console.log(total)
+        break
     }
-    console.log("")
 }
-
-// remove left/right slice. probably need to change left/right slice to slice on every third line
-// group input.txt into groupings of 3
-// find the common character between each grouping of 3 (keep but modify lines 20 and 21)
-// sum priorities
+}
+// [x] remove left/right slice. probably need to change left/right slice to slice on every third line
+// [x] group input.txt into groupings of 3
+// [] find the common character between each grouping of 3 (keep but modify lines 20 and 21)
+// [] sum priorities
