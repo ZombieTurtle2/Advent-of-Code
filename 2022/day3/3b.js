9b521ffea23061546539deaf1fb0e8615d6048ed
Tyler Wilcox <tyler.wlcx@gmail.com>
2023-02-02 23:22:06 -0700

    Finished 3b

--- 2022/day3/3b.js
+++ 2022/day3/3b.js
@@ -12,38 +12,19 @@ let lines = fs
     .split('\r\n');
 let total = 0
 
-// for (let line of lines) {
-//     let length = line.length;
-//     let left = line.slice(0, length / 2)
-//     let right = line.slice(length/2)
+let i = 0;
+while(i < lines.length) {
+    const elfGroup = lines.slice(i, i + 3);
 
-//     for (let char of left) {
-//         if (right.includes(char)) {
-//             console.log(char)
-//             total = total + priorities.indexOf(char)
-//             console.log(total)
-//             break
-//         }
-//     }
-//     console.log("")
-// }
-
-const groupSize = 3;
-const elfGrouping = [];
-
-for (let i = 0; i < lines.length; i += groupSize) {
-    const elfGroup = lines.slice(i, i + groupSize);
-    elfGrouping.push(elfGroup);
-
-for (let char of elfGroup) {
-    if (elfGroup.includes(char)) {
-        console.log(char)
-        total = total + priorities.indexOf(char)
-        console.log(total)
-        break
+    for (let char of elfGroup[0]) { 
+        if (elfGroup[1].includes(char) && elfGroup[2].includes(char)) {
+            total = total + priorities.indexOf(char)
+            break
+        }
     }
+    i = i + 3;
 }
-}
+console.log(total)
 // [x] remove left/right slice. probably need to change left/right slice to slice on every third line
 // [x] group input.txt into groupings of 3
 // [] find the common character between each grouping of 3 (keep but modify lines 20 and 21)
