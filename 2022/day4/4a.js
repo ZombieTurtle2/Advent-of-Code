7a9a671f7c0195faccb92b998e93dd3eee774dc5
Tyler Wilcox <tyler.wlcx@gmail.com>
2023-02-14 23:46:35 -0700

    Worked on day 4

--- 2022/day4/4a.js
+++ 2022/day4/4a.js
@@ -1,6 +1,14 @@
 //Imports the fs module
 const fs = require("fs")
 
+function genRange(start, end) {
+    let range = [];
+    for (let i = parseInt(start); i <= end; i++) {
+        range.push(i);
+    }
+    return range
+}
+
 let lines = fs
     //Open the file
     .readFileSync("input.txt")
@@ -9,16 +17,31 @@ let lines = fs
     //Split it on new lines (\r\n)
     .split('\r\n');
 
-    let i = 0;
-while(i < lines.length) {
-    const elfGroup = lines.slice(i, i + 3);
 
-    for (let char of elfGroup[0]) { 
-        if (elfGroup[1].includes(char) && elfGroup[2].includes(char)) {
-            total = total + priorities.indexOf(char)
-            break
+let total = 0
+
+for (let line of lines) {
+    let left = line.split(',')[0]
+    left = left.split('-')
+    let right = line.split(',')[1]
+    right = right.split('-')
+
+    left = genRange(left[0], left[1])
+    right = genRange(right[0], right[1])
+    // console.log(left)
+    // console.log(right)
+    // console.log("")
+
+    for (let num of left) {
+        console.log(`Right: ${right}`)
+        if (right.includes(num) == false) {
+            console.log(`Does not include: ${num}`)
+            continue
+        } else {
+            
         }
     }
-    i = i + 3;
-}
-console.log(total)
+};
+
+//Else if it does contain all numbers of left (true), +1
+//total = true + 1? 
